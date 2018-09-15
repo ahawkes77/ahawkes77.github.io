5c2b26c9b06432555b69184a7d75050bf304648e
Adrian Hawkes <hawkes.adrian@gmail.com>
2018-09-15 20:17:45 +0100

    change layout slightly, include react database project

---
+++ projects/ReactDB/README.md
@@ -0,0 +1,7 @@
+# React Database
+
+Basic database built using React.
+
+Code taken originally from tutorial by [Tania Rascia](https://www.taniarascia.com/getting-started-with-react/)
+
+
--- projects/quoteGenerator/script.js
+++ projects/quoteGenerator/script.js
@@ -5,27 +5,26 @@ function createQuote(q, num) {
     
      for (var p in q) {
       if (q.hasOwnProperty(p)) {
-        quote += ' '+ q[p][Math.floor(Math.random()*q[p].length)];
+        quote += ' '+ q[p][Math.floor(Math.random()*q[p].length)]
       }
     }
-
     document.getElementById("intro").innerHTML+=quote + "<br>";
   }
 }
 
 
 var firstTest = {
-    a: ["'I love you the more in that I believe you had liked me for my own sake and nothing else.' - John Keats ", "'There is nothing permanent except change.' - Heraclitus", "'Learning never exhausts the mind.' - Leonardo da Vinci", "'There is no charm equal to tenderness of heart.' - Jane Austen", "'Life without love is like a tree without blossoms or fruit.' - Khalil Gibran", "'Think in the morning. Act in the noon. Eat in the evening. Sleep in the night.' - William Blake"],
-    // b: ["please don't forget", "remember to bring", "you should take", "and the sun is shining,", "and the rain is falling,", "and a storm is coming," ],
-    // c: ["your umbrella.", "something nice to wear.", "a bunch of flowers.", "you might avoid getting wet.", "you might need to have a lie down.", "you will have some good luck today."]
+    a: ["When you go to the shops,", "If you go to the park,", "If you go out on Saturday night,", "If it is early in the morning,", "If you forget to bring an umbrella,", "If you think about it long enough,"],
+    b: ["please don't forget", "remember to bring", "you should take", "and the sun is shining,", "and the rain is falling,", "and a storm is coming," ],
+    c: ["your umbrella.", "something nice to wear.", "a bunch of flowers.", "you might avoid getting wet.", "you might need to have a lie down.", "you will have some good luck today."]
     
 };
 
 var secondTest = {
-  a: ["When you go to the shops,", "If you go to the park,", "If you go out on Saturday night,", "If it is early in the morning,", "If you forget to bring an umbrella,", "If you think about it long enough,"],
-  b: ["please don't forget", "remember to bring", "you should take", "and the sun is shining,", "and the rain is falling,", "and a storm is coming," ],
-  c: ["your umbrella.", "something nice to wear.", "a bunch of flowers.", "you might avoid getting wet.", "you might need to have a lie down.", "you will have some good luck today."]
-  
+    a:["If it is early in the morning,", "If you forget to bring an umbrella,", "If you think about it long enough,"],
+    b: ["and the sun is shining,", "and the rain is falling,", "and a storm is coming," ],
+    c: ["you might avoid getting wet.", "you might need to have a lie down.", "you will have some good luck today."]
+    
 };
 
 
