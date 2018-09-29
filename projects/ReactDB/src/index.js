652a7913dcdec5e3932eb8417ab10b0bf74343de
Adrian Hawkes <hawkes.adrian@gmail.com>
2018-09-29 20:49:30 +0100

    updating with new project

--- projects/ReactDB/src/index.js
+++ projects/ReactDB/src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import {HashRouter, Route, Link, Switch} from "react-router-dom";
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+export default () => (
+    <HashRouter>
+        <Switch>
+        </Switch>
+    </HashRouter>
+)
