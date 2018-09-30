6f4f1cec60ec35cce8d9c5e0b241ffc1ecaa6ce0
Adrian Hawkes <hawkes.adrian@gmail.com>
2018-09-30 09:43:46 +0100

    deleting duplicate files and amending some content

--- projects/quoteGenerator/script.js
+++ projects/quoteGenerator/script.js
@@ -15,7 +15,7 @@ function createQuote(q, num) {
 
 
 var firstTest = {
-    a: ["'I love you the more in that I believe you had liked me for my own sake and nothing else.' - John Keats ", "'There is nothing permanent except change.' - Heraclitus", "'Learning never exhausts the mind.' - Leonardo da Vinci", "'There is no charm equal to tenderness of heart.' - Jane Austen", "'Life without love is like a tree without blossoms or fruit.' - Khalil Gibran", "'Think in the morning. Act in the noon. Eat in the evening. Sleep in the night.' - William Blake"],
+    a: ["'I love you the more in that I believe you had liked me for my own sake and nothing else.' - John Keats ", "'There is nothing permanent except change.' - Heraclitus", "'Learning never exhausts the mind.' - Leonardo da Vinci", "'There is no charm equal to tenderness of heart.' - Jane Austen", "'Life without love is like a tree without blossoms or fruit.' - Khalil Gibran", "'Think in the morning. Act in the noon. Eat in the evening. Sleep in the night.' - William Blake", "'One ought, every day at least, to hear a little song, read a good poem, see a fine picture, and, if it were possible, to speak a few reasonable words.' - Johann Wolfgang von Goethe"],
     // b: ["please don't forget", "remember to bring", "you should take", "and the sun is shining,", "and the rain is falling,", "and a storm is coming," ],
     // c: ["your umbrella.", "something nice to wear.", "a bunch of flowers.", "you might avoid getting wet.", "you might need to have a lie down.", "you will have some good luck today."]
     
